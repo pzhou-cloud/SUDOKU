bba0516a52ceaa373fbc02be72759d929cfeaeee
adestal <adestal@ucm.es>
2026-03-11 12:31:16 +0100

    Ultimos cambios 11/03
    .

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -54,16 +54,19 @@ int dame_celda_bloqueadas(const tSudoku& s) {
 	return s.celdas_bloqueadas.cont; 
 
 }
-bool es_valor_posible(const tSudoku& s, int f, int c, int v) {
+bool es_valor_posible(tSudoku& s, int f, int c, int v) {
 	bool ok = false; 
-	if (s.tablero.matriz[f][c].estado != OCUPADA && s.tablero.matriz[f][c].estado != ORIGINAL) {
-		s.tablero.matriz[f][c].valor = v;
+	if (es_vacia(s.tablero.matriz[f][c]) && v >= 0 && v >= 9) {
+	
 		ok = true;
-	}// falta
-
+	}
+	return ok; 
 }
 
-void pon_valor(tSudoku& s, int f, int c, int v) {
+bool pon_valor(tSudoku& s, int f, int c, int v) {
+	
+	bool ok = false;
+	
 	if (es_valor_posible(s, f, c, v)) {
 
 		s.tablero.matriz[f][c].valor = v;
@@ -79,9 +82,16 @@ void pon_valor(tSudoku& s, int f, int c, int v) {
 		ok = true;
 	}
 
-
+	return ok;
 }
 void quita_valor(tSudoku& s, int f, int c) {
+
+	if (!es_vacia(dame_celda(s, f, c)) && !es_original(dame_celda(s, f, c)) && f >= 0 && f <= 9) {
+		
+		tCelda celda = dame_celda(s, f, c);
+		pon_vacia(celda);
+		//problema para el futuro
+	}
 
 }
 void reset(tSudoku& s);
