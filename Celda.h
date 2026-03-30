42184be45c8f8fd173a41e3ac082f8e18dee0b36
pzhou-cloud <pzhou@ucm.es>
2026-03-30 23:37:19 +0200

    Refactor Celda.h to improve function signatures

--- Celda.h
+++ Celda.h
@@ -1,22 +1,21 @@
 #pragma once
-
 enum tEstado { ORIGINAL, OCUPADA, VACIA };
 
 struct tCelda {
-	int valor;
-	tEstado estado;
+	int valor;  // valor existente en la celda
+	tEstado estado; // estado de la celda
 };
+using namespace std;
 
 void inicializaCelda(tCelda& c);
-void inicializaCelda(tCelda& c, int v, tEstado estado);
+void inicializaCelda(tCelda& c, int& v, tEstado& estado);
 
 bool es_vacia(const tCelda& c);
 bool es_original(const tCelda& c);
 bool es_ocupada(const tCelda& c);
-int dame_valor(const tCelda& c);
 
+int dame_valor(const tCelda& c);
 void pon_valor(tCelda& c, int v);
 void pon_vacia(tCelda& c);
 void pon_original(tCelda& c);
 void pon_ocupada(tCelda& c);
-
