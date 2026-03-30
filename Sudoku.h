edc29affa8cb128ad283e977e5500b99c6fde02b
pzhou-cloud <pzhou@ucm.es>
2026-03-30 23:39:34 +0200

    Refactor Sudoku.h by reorganizing function declarations

--- Sudoku.h
+++ Sudoku.h
@@ -1,7 +1,5 @@
-
 #pragma once
 #include "Tablero.h";
-
 #include <fstream>;
 using namespace std;
 struct tPosicion {
@@ -21,23 +19,18 @@ struct tSudoku {
 };
 
 void inicializaSudoku(tSudoku& s);
-
 void carga_sudoku(ifstream& archivo, tSudoku& s);
-
-
-bool terminado(tSudoku s);
-
-int dame_dimension(const tSudoku& s); 
+int dame_dimension(const tSudoku& s);
 tCelda dame_celda(const tSudoku s, int f, int c);
-bool terminado(const tSudoku& s); 
-bool bloqueo(const tSudoku& s); 
-int dame_celda_bloqueadas(const tSudoku& s); 
-bool es_valor_posible(const tSudoku& s, int f, int c, int v); 
 
-void pon_valor(tSudoku& s, int f, int c, int v); 
+bool terminado(const tSudoku& s);
+bool bloqueo(const tSudoku& s);
+int dame_celda_bloqueadas(const tSudoku& s);
+bool es_valor_posible(const tSudoku& s, int f, int c, int v);
+
+bool pon_valor(tSudoku& s, int f, int c, int v);
 void quita_valor(tSudoku& s, int f, int c);
-void reset(tSudoku& s); 
-void autocompleta(tSudoku& s); 
-
+void reset(tSudoku& s);
+void autocompleta(tSudoku& s);
 
 
